1c6ae5d33e5528c1bde5f733a49ce0673a7fae57
Hasan Raza <hasan@so.city>
2019-07-24 12:13:13 +0530

    added socket events

---
+++ constants.js
@@ -0,0 +1,6 @@
+const constants = {
+    MOUSE_MOVE: 'mouse_move',
+    MOUSE_CLICK: 'mouse_click'
+}
+
+module.exports = constants;
---
+++ socket.js
@@ -0,0 +1,20 @@
+const constants = require('./constants');
+
+
+function handleSocketEvents(socket, robot) {
+    socket.on(constants.MOUSE_MOVE, ({x, y, scroll}) => {
+        if(!scroll) {
+            const { x: X, y: Y } = robot.getMousePos();
+            robot.moveMouse(x + X, y + Y);
+        }else {
+            y = y > 0 ? 1 : -1;
+            robot.scrollMouse(0, y);
+        }
+    });
+
+    socket.on(constants.MOUSE_CLICK, ({ button, double }) => {
+        robot.mouseClick(button, double);
+    })
+}
+
+module.exports = handleSocketEvents;
